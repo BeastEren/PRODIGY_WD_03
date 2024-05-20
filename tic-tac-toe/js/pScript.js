2866889632eba879b45beaf45c00cc038b8abe30
Sourav Singh <souravsinghami1@gmail.com>
2024-05-20 15:30:18 +0530

    done

--- tic-tac-toe/js/pScript.js
+++ tic-tac-toe/js/pScript.js
@@ -1,6 +1,5 @@
 console.log("Welcome to Tic Tac Toe")
 let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
 let turn = "X"
 let isgameover = false;
@@ -56,7 +55,6 @@ Array.from(boxes).forEach(element =>{
         if(boxtext.innerText === ''){
             boxtext.innerText = turn;
             turn = changeTurn();
-            audioTurn.play();
             checkWin();
             if (!isgameover){
                 document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
